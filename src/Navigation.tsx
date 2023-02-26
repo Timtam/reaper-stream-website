efa7dc570c551564b2df781565c80ae06fcdfe03
Toni Barth <software@satoprogs.de>
2023-02-26 15:08:12 +0100

    add routes for offline and live page directly

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,8 +1,11 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import config from "./config.json";
 import Home from "./Home";
+import Live from "./Live";
 import Loading from "./Loading";
 import NotFound from "./NotFound";
+import Offline from "./Offline";
 import TTQuickstart from "./TTQuickstart";
 
 function Navigation() {
@@ -10,6 +13,11 @@ function Navigation() {
     <Suspense fallback={<Loading />}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/live" element={<Live ytUrl={config.ytUrl} />} />
+        <Route
+          path="/offline"
+          element={<Offline nextStream={config.nextStream} />}
+        />
         <Route path="/TTQuickstart" element={<TTQuickstart />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
