afaaf8995c379bf51b8503d41e822ca1e8c5bd7f
Scott Chesworth <scottchesworth@gmail.com>
2023-08-07 13:58:46 +0100

    Set up Getting Started with Compression

--- src/Offline.tsx
+++ src/Offline.tsx
@@ -49,7 +49,7 @@ function Offline({ nextStream }: OfflineProps) {
       </p>
       {nextStream && countdown ? (
         <p>
-          A live Q&A with Scott and JennyK will be starting in{" "}
+          ?Getting Started with Compression will be happening  in{" "}
           {humanizeDuration(countdown, {
             units: ["w", "d", "h", "m"],
             round: true,
@@ -64,9 +64,9 @@ function Offline({ nextStream }: OfflineProps) {
         hit refresh every few minutes, just to give any stray
         internet gremlins a bonk on the noggin and get your browser back on track.
       </p>
-      <h2>Next Meet-up: Saturday 29th July, 8PM UK, 3PM Eastern.</h2>
+      <h2>Next Meet-up: Saturday August 12th, 8PM UK, 3PM Eastern.</h2>
       <p>
-        Join us for "I'm Perplexed, What's Next", the live meet-up where Scott and JennyK are on hand to answer questions about REAPER, OSARA, or pretty much anything around accessible recording and production. Questions from newcomers always get top priority in these sessions, it's a fun and friendly way to pick up new knowledge.
+        Scott, JennyK and Justin are teaming up to help get you started with compression. We'll be breaking down a bunch of terminology, familiarising you with the most commonly used controls and of course, there will be plenty of audio to listen to along the way, plus breaks for Q&A.
       </p>
       <p>
         <a href="/REAPERMadeEasyMeet-ups.ics" target="_blank" rel="noreferrer">
